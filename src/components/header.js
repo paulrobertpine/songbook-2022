0d4f548f9d03a1f84c24d1b2d38be7e41ff2a417
paulrobertpine <paulrobertpine@gmail.com>
2022-05-03 09:16:59 -0400

    advance button instead of scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,61 +1,34 @@
 import React from "react"
 import Link from "gatsby-link"
+import * as Scroll from "react-scroll"
 import HeaderIcon from "../images/musician.inline.svg"
-import { IoPlayCircle, IoPauseCircle } from "react-icons/io5"
-import Slider from "rc-slider"
-import "rc-slider/assets/index.css"
+import { IoArrowDownCircle } from "react-icons/io5"
 
-let scrolldelay = -1
-const defaultSpeed = 50
+let scroll = Scroll.animateScroll
+
+// set duration based on slider?
+// calculate an amount to scroll based on viewport height?
+// or use slider to set amount to scroll by
+
+function scrollPage() {
+  console.log("yep")
+  scroll.scrollMore(300, {
+    duration: 1500,
+    smooth: "linear",
+  })
+}
 
 export default function Header() {
-  const [scrolling, setScrolling] = React.useState(false)
-  const [speed, setSpeed] = React.useState(defaultSpeed)
-
-  function toggleScroll(bool) {
-    setScrolling(bool)
-  }
-
-  function setScrollSpeed(e) {
-    setSpeed(e)
-  }
-
-  // function scroll() {
-  //   console.log(speed)
-  //   window.scrollBy(0, 1)
-  //   scrolldelay = setTimeout(scroll, speed)
-  // }
-
-  function stopScrolling() {
-    window.clearTimeout(scrolldelay)
-    scrolldelay = -1
-  }
-
-  // if (scrolling) {
-  //   scroll()
-  // } else {
-  //   stopScrolling()
-  // }
-
   return (
     <header id="site-header">
       <section className="container">
-        <Link to="/" id="site-logo" onClick={stopScrolling}>
+        <Link to="/" id="site-logo">
           <HeaderIcon />
         </Link>
         <nav id="scroll-control">
-          <Slider
-            className="speed-slider"
-            defaultValue={50}
-            reverse={true}
-            onChange={stopScrolling}
-            onAfterChange={setScrollSpeed}
-          />
-          {scrolling ? (
-            <IoPauseCircle onClick={() => toggleScroll(false)} />
-          ) : (
-            <IoPlayCircle onClick={() => toggleScroll(true)} />
-          )}
+          <button onClick={() => scrollPage()}>
+            <IoArrowDownCircle />
+          </button>
         </nav>
       </section>
     </header>
